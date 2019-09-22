5e0c2add1a686b987874612ac166b840010439b9
Miguel Lopes <miguel.lopes@miniclip.com>
2019-09-22 22:56:39 +0100

    intro to material system

--- OPENGL GLAD/main.cpp
+++ OPENGL GLAD/main.cpp
@@ -196,6 +196,10 @@ int main()
         lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
+        lightingShader.setVec3("material.ambient",  1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.diffuse",  1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setFloat("material.shininess", 32.0f);
 
 
         // view/projection transformations
