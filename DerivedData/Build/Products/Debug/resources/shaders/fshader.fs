6b80532d35ae164b842fb7361dbd569185cf2a6a
Miguel Lopes <miguel.lopes@miniclip.com>
2019-10-25 22:38:53 +0100

    add refraction ability

--- DerivedData/Build/Products/Debug/resources/shaders/fshader.fs
+++ DerivedData/Build/Products/Debug/resources/shaders/fshader.fs
@@ -7,9 +7,16 @@ in vec3 Position;
 uniform vec3 cameraPos;
 uniform samplerCube skybox;
 
+//void main()
+//{
+//    vec3 I = normalize(Position - cameraPos);
+//    vec3 R = reflect(I, normalize(Normal));
+//    FragColor = vec4(texture(skybox, R).rgb, 1.0);
+//}
 void main()
 {
+    float ratio = 1.00 / 1.1;
     vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+    vec3 R = refract(I, normalize(Normal), ratio);
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
 }
